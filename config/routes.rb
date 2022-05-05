c5d2a53eeb55c0eb0b36b14edc36346e767cda93
EC2 Default User <ec2-user@ip-172-31-40-255.ec2.internal>
2022-05-05 09:23:47 +0000

    Rails6-bookers2-debug-main[Finish]

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,13 @@ Rails.application.routes.draw do
 
   root to: "homes#top"
   get "home/about"=>"homes#about"
+  get "search" => "searches#search_result"
 
   resources :books do
   resource :favorites, only: [:create, :destroy]
   resources :book_comments, only: [:create, :destroy]
   end
-  
+
   resources :users do
     resource :relationships, only: [:create, :destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
